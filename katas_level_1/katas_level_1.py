5085dfa49f6c0aa0d1d1eeb72496a110d9873d1b
rokhuda <rokhuda@gmail.com>
2020-02-12 21:15:30 +0200

    first commit

---
+++ katas_level_1/katas_level_1.py
@@ -0,0 +1,15 @@
+def hello(name):
+    return f"Hello {name}"
+
+print(hello('Rokhuda'))
+
+def even_or_odd(num):
+    if (num%2) == 0:
+        return 'even'
+    else:
+        return 'odd'
+
+print(even_or_odd(3))
+
+
+
