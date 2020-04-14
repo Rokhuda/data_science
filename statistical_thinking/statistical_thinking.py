5deecf331fce7e0def290a418d7ee900577c8511
rokhuda <rokhuda@gmail.com>
2020-04-14 09:11:33 +0200

    done with question 1

--- statistical_thinking/statistical_thinking.py
+++ statistical_thinking/statistical_thinking.py
@@ -4,15 +4,20 @@ import seaborn as sb
 import pandas as pd
 import scipy as sp
 
-df = pd.read_csv('MDG_Export_20191227.csv')
-print(df.head())
+data = pd.read_csv('MDG_Export_20191227.csv')
+df = pd.DataFrame(data ,columns=['Country', 'year', 'series'])
+# print(data.describe())
 
 # 1.How many different countries are represented? How many missing values are there by country, year and series?
 
 """
-How different  countruies are repreesented
+How different  countruies are represented
 """
-df['Country'].count()
-print(df)
+df_num_countries = len(df.groupby('Country'))
+print(df_num_countries)
 
+"""
+How many missing values are there by country, year and series?
 
+"""
+print((len(df[df.isnull().any(axis=1)])))
